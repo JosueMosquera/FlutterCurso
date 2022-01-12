1f87211c87bd63754467d997d9c047207f54cd46
Curipoma <alvarocuripoma@gmail.com>
2022-01-12 12:00:07 -0500

    ...

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final options = const [
+    'title 1',
+    'title 2',
+    'title 3',
+    'title 4',
+    'title 5',
+    'title 6',
+    'title 7',
+    'title 8',
+    'title 9',
+    'title 10',
+  ];
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('List view tipo 1'),
+      ),
+      // body: ListView.builder(itemBuilder: options)
+    );
+  }
+}

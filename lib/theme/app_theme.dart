0daa865bec7bb3bee0f4f4d6e14aad9282803a7e
Curipoma <alvarocuripoma@gmail.com>
2022-01-14 12:25:12 -0500

    sadsd

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static const Color primary = Colors.pink;
+  static const Color primary = Colors.green;
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     primaryColor: primary,
     appBarTheme: const AppBarTheme(color: primary, elevation: 0),
-    textButtonTheme: const  TextButtonThemeData(style: ButtonStyle()),
+    textButtonTheme:
+        TextButtonThemeData(style: TextButton.styleFrom(primary: Colors.white)),
   );
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-    primaryColor: primary,
-    appBarTheme: const AppBarTheme(color: primary, elevation: 0),
-    scaffoldBackgroundColor: Colors.black
-  );
+      primaryColor: primary,
+      appBarTheme: const AppBarTheme(color: primary, elevation: 0),
+      scaffoldBackgroundColor: Colors.black);
 }
